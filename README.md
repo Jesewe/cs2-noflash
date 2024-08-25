966266ac52e1405063c45e24316f46b84a1cd299
ItsJesewe <81848878+Jesewe@users.noreply.github.com>
2024-08-25 15:12:35 +0500

    Refactored script

--- README.md
+++ README.md
@@ -1,78 +1,69 @@
-# Counter-Strike 2 NoFlash Script
+<div align="center">
+   <img src="src/img/icon.png" alt="CS2 NoFlash" width="200" height="200">
+   <h1>🌟 CS2 NoFlash 🌟</h1>
+   <p>Your ultimate anti-flash tool for Counter-Strike 2</p>
+   <a href="#features"><strong>Features</strong></a> •
+   <a href="#installation"><strong>Installation</strong></a> •
+   <a href="#usage"><strong>Usage</strong></a> •
+   <a href="#customization"><strong>Customization</strong></a> •
+   <a href="#troubleshooting"><strong>Troubleshooting</strong></a> •
+   <a href="#contributing"><strong>Contributing</strong></a>
+   <br><br>
+   <p><strong>🌍 Translations:</strong></p>
+   <a href="README.ru.md"><img src="https://img.shields.io/badge/lang-Russian-purple?style=for-the-badge&logo=googletranslate"></a>
+   <a href="README.fr.md"><img src="https://img.shields.io/badge/lang-French-purple?style=for-the-badge&logo=googletranslate"></a>
+   <a href="README.es.md"><img src="https://img.shields.io/badge/lang-Spanish-purple?style=for-the-badge&logo=googletranslate"></a>
+   <a href="README.uk-UA.md"><img src="https://img.shields.io/badge/lang-Ukrainian-purple?style=for-the-badge&logo=googletranslate"></a>
+   <a href="README.pl.md"><img src="https://img.shields.io/badge/lang-Polish-purple?style=for-the-badge&logo=googletranslate"></a>
+</div>
 
-This script disables the flashbang effect in Counter-Strike 2 by continuously setting the flash alpha value to zero.
+---
+
+# Overview
+CS2 NoFlash is an automated tool designed for Counter-Strike 2 that prevents the player from being fully flashed by automatically adjusting the flashbang alpha values in the game.
 
 ## Features
-
-- Continuously sets `m_flFlashMaxAlpha` to 0 to disable flashbang effects.
-- Fetches the latest offsets from a remote source.
-- Provides real-time logging and feedback.
-- Graceful termination with `KeyboardInterrupt`.
-
-## Requirements
-
-- Python 3.7+
-- `pymem`
-- `requests`
-- `packaging`
+- **Anti-Flash Protection:** Automatically sets the flashbang alpha value to 0, preventing the player from being fully blinded.
+- **Process Attachment:** Attaches to the `cs2.exe` process and reads memory values to apply real-time changes.
+- **Update Checker:** Automatically checks for the latest version and notifies the user if an update is available.
+- **Error Logging:** Logs errors and important events to a log file for debugging purposes.
 
 ## Installation
-
-1. Clone the repository or download the script.
-
-   ```sh
+1. **Clone the Repository:**
+   ```bash
    git clone https://github.com/Jesewe/cs2-noflash.git
    cd cs2-noflash
    ```
 
-2. Install the required Python libraries.
-
-   ```sh
-   pip install pymem requests packaging
+2. **Install Dependencies:**
+   ```bash
+   pip install -r requirements.txt
    ```
 
-3. Run the script.
-
-   ```sh
+3. **Run the Script:**
+   ```bash
    python main.py
    ```
 
 ## Usage
+1. Ensure that Counter-Strike 2 is running.
+2. Execute the script using the command above.
+3. The script will automatically check for updates and fetch the necessary offsets from the provided sources.
+4. The NoFlash protection will start automatically, reducing the flashbang effect to minimal.
 
-1. Start Counter-Strike 2.
-2. Run the `noflash.py` script.
-3. The script will continuously disable the flash effect while the game is running.
-4. To stop the script, simply close the console window or press `Ctrl+C`.
-
-## Code Overview
-
-The script performs the following steps:
-
-1. Fetches the latest offsets and client data from a remote source using the `fetch_offsets` function.
-2. Initializes `Pymem` to access the game's memory using the `initialize_pymem` function.
-3. Continuously checks and updates the `m_flFlashMaxAlpha` value to 0 using the `noflash_loop` function, ensuring the flash effect is constantly disabled.
-
-### Functions
-
-- **fetch_offsets**: Fetches offsets and client data from a remote JSON source.
-- **initialize_pymem**: Initializes `Pymem` and retrieves the base address of `client.dll`.
-- **noflash_loop**: Continuously sets `m_flFlashMaxAlpha` to 0 to disable the flashbang effect.
-- **main**: Orchestrates the flow of the script.
-
-## Logging
-
-The script uses the `logging` module to log important events and errors. The logs include timestamps and error messages, which can be helpful for debugging.
+## Customization
+- **Log Directory:** The log files are saved in the `%LOCALAPPDATA%\Requests\ItsJesewe\crashes` directory by default. You can change this by modifying the `LOG_DIRECTORY` variable in the script.
 
 ## Troubleshooting
+- **Failed to Fetch Offsets:** Ensure you have an active internet connection and that the source URLs are accessible.
+- **Could Not Open `cs2.exe`:** Make sure the game is running and that you have the necessary permissions.
+- **Unexpected Errors:** Check the log file located in the log directory for more details.
 
-- Ensure that Counter-Strike 2 is running and the window title matches `Counter-Strike 2`.
-- Make sure you run the script with sufficient permissions to access the game's memory.
-- Verify that the offsets are up to date. If the game updates, the offsets might change, and you will need to fetch the latest ones.
+## Contributing
+Contributions are welcome! Please open an issue or submit a pull request on the [GitHub repository](https://github.com/Jesewe/cs2-noflash).
 
 ## Disclaimer
-
 This script is for educational purposes only. Using cheats or hacks in online games is against the terms of service of most games and can result in bans or other penalties. Use this script at your own risk.
 
 ## License
-
 This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
--- main.py
+++ main.py
@@ -4,80 +4,175 @@ import time
 import logging
 from requests import get
 from packaging import version
+from colorama import init, Fore
+import json
+import os
+import ctypes
 
-VERSION = "v1.0.6"
+# Initialize colorama for colored console output
+init(autoreset=True)
 
-def fetch_offsets():
-    try:
-        offset = get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.json").json()
-        client = get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client_dll.json").json()
-        return offset, client
-    except Exception as e:
-        logging.error(f"Failed to fetch offsets: {e}")
-        return None, None
+class Logger:
+    """Handles logging setup for the application."""
+    
+    LOG_DIRECTORY = os.path.expandvars(r'%LOCALAPPDATA%\Requests\ItsJesewe\crashes')
+    LOG_FILE = os.path.join(LOG_DIRECTORY, 'nf_logs.log')
 
-def initialize_pymem():
-    try:
-        pm = pymem.Pymem("cs2.exe")
-        return pm
-    except pymem.exception.PymemError as e:
-        logging.error(f"Could not open cs2.exe: {e}")
-        return None
+    @staticmethod
+    def setup_logging():
+        """Set up the logging configuration."""
+        os.makedirs(Logger.LOG_DIRECTORY, exist_ok=True)
+        with open(Logger.LOG_FILE, 'w') as f:
+            pass
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(levelname)s: %(message)s',
+            handlers=[logging.FileHandler(Logger.LOG_FILE), logging.StreamHandler()]
+        )
 
-def check_for_updates():
-    try:
-        response = get("https://api.github.com/repos/Jesewe/cs2-noflash/tags")
-        response.raise_for_status()
-        latest_version = response.json()[0]["name"]
-        if version.parse(latest_version) > version.parse(VERSION):
-            logging.info(f"New version available: {latest_version}. Please update for the latest fixes and features.")
-        else:
-            logging.info("You are using the latest version.")
-    except Exception as e:
-        logging.error(f"Error checking for updates: {e}")
+class Utility:
+    """Contains utility functions for the application."""
 
-def get_client_module(pm):
-    client_module = pymem.process.module_from_name(pm.process_handle, "client.dll")
-    if not client_module:
-        logging.error("Could not find client.dll module.")
-        return None
-    return client_module.lpBaseOfDll
+    CACHE_DIRECTORY = os.path.expandvars(r'%LOCALAPPDATA%\Requests\ItsJesewe')
+    CACHE_FILE = os.path.join(CACHE_DIRECTORY, 'offsets_cache.json')
+    
+    @staticmethod
+    def set_console_title(title):
+        """Sets the console window title."""
+        ctypes.windll.kernel32.SetConsoleTitleW(title)
 
-def noflash_loop(pm, client_module, dwLocalPlayerPawn, m_flFlashMaxAlpha):
-    try:
-        while True:
-            player_position = pm.read_longlong(client_module + dwLocalPlayerPawn)
-            if player_position:
-                pm.write_float(player_position + m_flFlashMaxAlpha, 0.0)
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        logging.info("NoFlash script terminated by user.")
-    except Exception as e:
-        logging.error(f"Unexpected error: {e}")
+    @staticmethod
+    def fetch_offsets():
+        """Fetches offsets and client data from remote sources or local cache."""
+        try:
+            response_offset = get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/offsets.json")
+            response_client = get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/output/client_dll.json")
+            
+            if response_offset.status_code != 200 or response_client.status_code != 200:
+                logging.error(f"{Fore.RED}Failed to fetch offsets from server.")
+                return None, None
 
-def main():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    check_for_updates()
-    logging.info("Fetching offsets and client data...")
-    offsets, client_data = fetch_offsets()
-    if offsets is None or client_data is None:
-        return
+            offset = response_offset.json()
+            client = response_client.json()
 
-    global dwLocalPlayerPawn, m_flFlashMaxAlpha
-    dwLocalPlayerPawn = offsets["client.dll"]["dwLocalPlayerPawn"]
-    m_flFlashMaxAlpha = client_data["client.dll"]["classes"]["C_CSPlayerPawnBase"]["fields"]["m_flFlashMaxAlpha"]
+            if os.path.exists(Utility.CACHE_FILE):
+                with open(Utility.CACHE_FILE, 'r') as f:
+                    cached_data = json.load(f)
+                
+                if cached_data.get('offsets') != offset or cached_data.get('client') != client:
+                    logging.info(f"{Fore.YELLOW}Offsets have changed, updating cache...")
+                    with open(Utility.CACHE_FILE, 'w') as f:
+                        json.dump({'offsets': offset, 'client': client}, f)
+                else:
+                    logging.info(f"{Fore.CYAN}Using cached offsets.")
+                    return cached_data['offsets'], cached_data['client']
+            else:
+                os.makedirs(Utility.CACHE_DIRECTORY, exist_ok=True)
+                with open(Utility.CACHE_FILE, 'w') as f:
+                    json.dump({'offsets': offset, 'client': client}, f)
 
-    logging.info("Searching for cs2.exe process...")
-    pm = initialize_pymem()
-    if pm is None:
-        return
+            return offset, client
+        except Exception as e:
+            logging.error(f"{Fore.RED}Failed to fetch offsets: {e}")
+            logging.error(f"{Fore.RED}Please report this issue on the GitHub repository: https://github.com/Jesewe/cs2-noflash/issues")
+            return None, None
 
-    client_base = get_client_module(pm)
-    if client_base is None:
-        return
+    @staticmethod
+    def check_for_updates(current_version):
+        """Checks for software updates on GitHub."""
+        try:
+            response = get("https://api.github.com/repos/Jesewe/cs2-noflash/tags")
+            response.raise_for_status()
+            latest_version = response.json()[0]["name"]
+            if version.parse(latest_version) > version.parse(current_version):
+                logging.warning(f"{Fore.YELLOW}New version available: {latest_version}. Please update for the latest fixes and features.")
+            else:
+                logging.info(f"{Fore.GREEN}You are using the latest version.")
+        except Exception as e:
+            logging.error(f"{Fore.RED}Error checking for updates: {e}")
 
-    logging.info("cs2.exe is running.")
-    noflash_loop(pm, client_base, dwLocalPlayerPawn, m_flFlashMaxAlpha)
+class PymemHandler:
+    """Handles interaction with the game process using pymem."""
 
-if __name__ == "__main__":
-    main()
+    def __init__(self, process_name="cs2.exe"):
+        self.pm = None
+        self.client_base = None
+        self.process_name = process_name
+
+    def initialize_pymem(self):
+        """Initializes Pymem and attaches to the game process."""
+        try:
+            self.pm = pymem.Pymem(self.process_name)
+        except pymem.exception.PymemError as e:
+            logging.error(f"{Fore.RED}Could not open {self.process_name}: {e}")
+            return False
+        return True
+
+    def get_client_module(self, module_name="client.dll"):
+        """Retrieves the client.dll module base address."""
+        client_module = pymem.process.module_from_name(self.pm.process_handle, module_name)
+        if not client_module:
+            logging.error(f"{Fore.RED}Could not find {module_name} module.")
+            return False
+        self.client_base = client_module.lpBaseOfDll
+        return True
+
+class NoFlashScript:
+    """Main class for the CS2 NoFlash functionality."""
+    
+    VERSION = "v1.0.7"
+
+    def __init__(self):
+        """Initializes the NoFlashScript with necessary attributes."""
+        self.pymem_handler = PymemHandler()
+        self.dwLocalPlayerPawn = None
+        self.m_flFlashMaxAlpha = None
+        self.is_running = False
+
+    def start(self):
+        """Starts the main loop of the NoFlashScript."""
+        Utility.set_console_title(f"CS2 NoFlash {self.VERSION}")
+        Utility.check_for_updates(self.VERSION)
+        logging.info(f"{Fore.CYAN}Fetching offsets and client data...")
+
+        offsets, client_data = Utility.fetch_offsets()
+        if offsets is None or client_data is None:
+            input(f"{Fore.RED}Press Enter to exit...")
+            return
+
+        # Set offsets and client data
+        self.dwLocalPlayerPawn = offsets["client.dll"]["dwLocalPlayerPawn"]
+        self.m_flFlashMaxAlpha = client_data["client.dll"]["classes"]["C_CSPlayerPawnBase"]["fields"]["m_flFlashMaxAlpha"]
+        
+        logging.info(f"{Fore.CYAN}Searching for cs2.exe process...")
+        if not self.pymem_handler.initialize_pymem():
+            input(f"{Fore.RED}Press Enter to exit...")
+            return
+
+        if not self.pymem_handler.get_client_module():
+            input(f"{Fore.RED}Press Enter to exit...")
+            return
+
+        logging.info(f"{Fore.GREEN}NoFlash script started.")
+        self.is_running = True
+        self.run_noflash_loop()
+
+    def run_noflash_loop(self):
+        """Runs the main loop of the NoFlash script."""
+        try:
+            while self.is_running:
+                player_position = self.pymem_handler.pm.read_longlong(self.pymem_handler.client_base + self.dwLocalPlayerPawn)
+                if player_position:
+                    self.pymem_handler.pm.write_float(player_position + self.m_flFlashMaxAlpha, 0.0)
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            logging.info(f"{Fore.YELLOW}NoFlash script terminated by user.")
+            self.is_running = False
+        except Exception as e:
+            logging.error(f"{Fore.RED}Unexpected error: {e}")
+            input(f"{Fore.RED}Press Enter to exit...")
+
+if __name__ == '__main__':
+    Logger.setup_logging()
+    noflash = NoFlashScript()
+    noflash.start()
